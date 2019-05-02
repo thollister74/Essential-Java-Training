d5d78867c1b743e579baca67325ec5b1e14d7514
thollister74 <49489585+thollister74@users.noreply.github.com>
2019-05-02 15:55:20 -0400

    - Created some simple print line statements for review - Created a scanner class to take in a value and kick out a response based on user feedback through an if else  - Learned the OR operator and Strings don't work together easily  - Enhanced the if else to make an elseIf section  - Let Intellij convert the if/else to a switch

--- declareVariableMinipulateOutputResults/src/Main.java
+++ declareVariableMinipulateOutputResults/src/Main.java
@@ -1,6 +1,6 @@
-//Created by hollisti on 2019-05-02.
 package src;
-//*;
+//Created by hollisti on 2019-05-02.
+
 import java.util.Scanner;
 
 public class Main {
@@ -17,11 +17,34 @@ public class Main {
         Scanner scan = new Scanner(System.in);
 
         String fromMichigan = scan.nextLine();
-        if (fromMichigan.equals ("Y")||fromMichigan.equals("y")) {
-            System.out.println("Glad you're not a Buckeye");
-        }  else {
-            System.out.println("Ew, I hope your not from Ohio.");
+        switch (fromMichigan) {
+            case "Y":
+            case "y":
+                System.out.println("Glad you're not a Buckeye");
+
+                break;
+            case "N":
+            case "n":
+                System.out.println("Ew, I hope your not from Ohio.");
+
+                break;
+            default:
+                System.out.println("Dude?! 'Y' or 'N'! It's not that hard! \n Press ENTER and try again!");
+
+                break;
         }
+// This is the if-else statement I created, the Switch statement above was set up by Intellij
+//*****  String fromMichigan = scan.nextLine();
+//        if (fromMichigan.equals("Y") || fromMichigan.equals("y")) {
+//            System.out.println("Glad you're not a Buckeye");
+//
+//        } else if (fromMichigan.equals("N") || fromMichigan.equals("n")) {
+//            System.out.println("Ew, I hope your not from Ohio.");
+//
+//        } else {
+//            System.out.println("Dude?! 'Y' or 'N'! It's not that hard!");
+//
+//*****   }
 
 
     }
