b8b6cb2f161e93e274c34a4d43ddd1f882ef053a
thollister74 <49489585+thollister74@users.noreply.github.com>
2019-05-12 16:41:38 -0400

    - Created some simple print line statements for review - Created a scanner class to take in a value and kick out a response based on user feedback through an if else  - Learned the OR operator and Strings don't work together easily  - Enhanced the if else to make an elseIf section  - Let Intellij convert the if/else to a switch  2019-05-12  - Created candyBar array, simple actions using that array

--- declareVariableMinipulateOutputResults/src/Main.java
+++ declareVariableMinipulateOutputResults/src/Main.java
@@ -1,5 +1,8 @@
-package src;
-//Created by hollisti on 2019-05-02.
+//**********************************************************//
+// Created by hollisti on 2019-05-02.                       //
+// Goal was to review some baics covered in videos          //
+// and try to set up some a conditional if else             //
+//**********************************************************//
 
 import java.util.Scanner;
 
@@ -10,42 +13,54 @@ public class Main {
         String lName = "Archer";
         int userAge = 34;
 
-        System.out.println ("Your first name: "+fName);
-        System.out.println("Your first name: "+lName);
-        System.out.println(("You are ") + userAge +( " years old"));
+        System.out.println("Your first name: " + fName);
+        System.out.println("Your first name: " + lName);
+        System.out.println(("You are ") + userAge + (" years old"));
         System.out.println("Are you from Michigan? Y/N");
         Scanner scan = new Scanner(System.in);
-
-        String fromMichigan = scan.nextLine();
-        switch (fromMichigan) {
-            case "Y":
-            case "y":
-                System.out.println("Glad you're not a Buckeye");
-
-                break;
-            case "N":
-            case "n":
-                System.out.println("Ew, I hope your not from Ohio.");
-
-                break;
-            default:
-                System.out.println("Dude?! 'Y' or 'N'! It's not that hard! \nPress ENTER and try again!");
-
-                break;
+        String []candyBar = {"Baby Ruth", "Twix", "Crunch", "Snickers"};
+        System.out.println(candyBar[2]);
+        candyBar[2] = "Munch";
+        System.out.println(candyBar.length);
+        for (int i = 0; i < candyBar.length; i++) {
+            System.out.println(candyBar[i]);
         }
-// This is the if-else statement I created, the Switch statement above was set up by Intellij
-//*****  String fromMichigan = scan.nextLine();
-//        if (fromMichigan.equals("Y") || fromMichigan.equals("y")) {
-//            System.out.println("Glad you're not a Buckeye");
+        // System created Switch statement
+        String isFromMichigan = scan.nextLine();
+        isFromMichigan = isFromMichigan.toUpperCase();
+
+                switch (isFromMichigan) {
+                    case "Y":
+                    System.out.println("Glad you're not a Buckeye> Have you been to the UP?");
+                    break;
+
+                    case "N":
+                    System.out.println("Ew, I hope your not from Ohio.");
+                    break;
+
+                    default:
+                    System.out.println("Enter 'Y' or 'N'! \nPress the green arrow and try again!");
+                    break;
+
+           //**********************************************************************************************
+           }
+
+//        // This is the if-else statement I created, the Switch statement above was set up by Intellij
+//        String fromMichigan = scan.nextLine();
+//        fromMichigan = fromMichigan.toUpperCase();
+//        if (fromMichigan.equals("Y")) {
+//            System.out.println("Glad you're not a Buckeye. Have you been to the UP?" +
+//                    "");
 //
-//        } else if (fromMichigan.equals("N") || fromMichigan.equals("n")) {
+//        } else if (fromMichigan.equals("N")) {
 //            System.out.println("Ew, I hope your not from Ohio.");
 //
 //        } else {
 //            System.out.println("Dude?! 'Y' or 'N'! It's not that hard!");
 //
-//*****   }
+//        }
 
 
     }
 }
+
