502a1811bc8e7ca2af9435c23b48653334e9ca35
thollister74 <49489585+thollister74@users.noreply.github.com>
2019-05-13 15:07:04 -0400

    - Created some simple print line statements for review - Created a scanner class to take in a value and kick out a response based on user feedback through an if else  - Learned the OR operator and Strings don't work together easily  - Enhanced the if else to make an elseIf section  - Let Intellij convert the if/else to a switch  2019-05-12  - Created candyBar array, simple actions using that array

---
+++ declareVariableMinipulateOutputResults/src/tutorialClass.java
@@ -0,0 +1,13 @@
+
+// Created by hollisti on 2019-05-02.
+
+import java.util.Scanner;
+
+public class tutorialClass {
+    public static void myVariables() {
+        String fName;
+        String lName;
+        int age = 34;
+        boolean isMichiganResident;
+    }
+}
